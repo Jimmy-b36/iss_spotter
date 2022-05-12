488354506921b7b8d88419aead323482f4682dde
James Ball <jamie.j.ball@gmail.com>
2022-05-12 14:52:35 -0700

    test file for iss.js

---
+++ test/issTest.js
@@ -0,0 +1,10 @@
+const { fetchMyIp } = require("../iss");
+const assert = require("chai");
+
+describe("#iss:", () => {
+  it("should return an ip if one is found", (done) => {
+    assert.equal(err, null);
+
+    done();
+  });
+});
