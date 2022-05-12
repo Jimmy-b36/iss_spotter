ead84f05f38fac19b7f64f9f11be8c6997798ba4
James Ball <jamie.j.ball@gmail.com>
2022-05-12 14:31:45 -0700

    functions to collect ip and location

---
+++ iss.js
@@ -0,0 +1,33 @@
+const request = require("request");
+let ip;
+const fetchMyIp = (callback) => {
+  request(`https://api64.ipify.org?format=json`, (err, response, body) => {
+    if (err) return callback(err, null);
+    if (response.statusCode !== 200) {
+      const msg = `Status code ${response.statusCode} when fetching IP. Response: ${body}`;
+      return callback(Error(msg), null);
+    }
+    ip = JSON.parse(body).ip;
+    // callback(null, ip);
+    return ip;
+  });
+};
+const fetchLocation = (ip, callback) => {
+  request(
+    `https://api.ipbase.com/v2/info?apikey=rZRoWBTYuRQlrPsdB5gKNvT3TSAiXaLUthy775jG&ip=${ip}`,
+    (err, response, body) => {
+      if (err) return callback(err, null);
+      if (response.statusCode !== 200) {
+        const msg = `Status code ${response.statusCode} when fetching location. Response: ${body}`;
+        return callback(Error(msg), null);
+      }
+      const location = {};
+      location.latitude = JSON.parse(body).data.location.latitude;
+      location.longitude = JSON.parse(body).data.location.longitude;
+
+      callback(null, location);
+    }
+  );
+};
+
+module.exports = { fetchMyIp, fetchLocation };
