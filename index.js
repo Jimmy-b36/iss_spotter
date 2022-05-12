a4012fa9572a3330665420e1b721e4c6f9229dc8
James Ball <jamie.j.ball@gmail.com>
2022-05-12 14:53:00 -0700

    added flyover times

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchMyIp, fetchLocation } = require("./iss");
+const { fetchMyIp, fetchLocation, issFlyover } = require("./iss");
 
 fetchMyIp((error, ip) => {
   if (error) console.log("Couldn't find ip", error);
@@ -6,5 +6,13 @@ fetchMyIp((error, ip) => {
 });
 fetchLocation("50.68.190.35", (error, location) => {
   if (error) console.log("Couldn't find location", error);
-  console.log(error, location);
+  console.log("Location: ", location);
 });
+
+issFlyover(
+  { latitude: "50.238258361816406", longitude: "-119.27671813964844" },
+  (error, flyOver) => {
+    if (error) console.log("Couldn't find flyover", error);
+    console.log("Fly Over: ", flyOver);
+  }
+);
