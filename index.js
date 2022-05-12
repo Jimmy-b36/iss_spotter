ef7840b07bd8ca3303049fb2f69ca4c3e08cf38b
James Ball <jamie.j.ball@gmail.com>
2022-05-12 15:31:51 -0700

    removed all uneccesary functions

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 const { fetchMyIp, fetchLocation, issFlyover } = require("./iss");
+const { nextIssFlyTime } = require("./iss");
 
-fetchMyIp((error, ip) => {
-  if (error) console.log("Couldn't find ip", error);
-  console.log("It worked!", ip);
-});
-fetchLocation("50.68.190.35", (error, location) => {
-  if (error) console.log("Couldn't find location", error);
-  console.log("Location: ", location);
-});
-
-issFlyover(
-  { latitude: "50.238258361816406", longitude: "-119.27671813964844" },
-  (error, flyOver) => {
-    if (error) console.log("Couldn't find flyover", error);
-    console.log("Fly Over: ", flyOver);
+nextIssFlyTime((error, passTimes) => {
+  if (error) return console.log("It didn't work", error);
+  for (time in passTimes) {
+    console.log(
+      `Next pass at ${Date(passTimes[time].risetime)} for ${
+        passTimes[time].duration
+      } seconds`
+    );
   }
-);
+});
